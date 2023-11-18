634d2ba73056b56fc7db178b00af2dfe8dce7718
CLARISSA CHEN <clarissachen5@gmail.com>
2023-11-18 18:10:16 -0500

    button css formatting updates

--- remixapp/app/components/button.tsx
+++ remixapp/app/components/button.tsx
@@ -1,3 +1,7 @@
 export const Button=(props:any) => {
-    return <button className="button" onClick={props.onClick}>{props.children}</button>
+    return <button className={`button ${props.mode}`} onClick={props.onClick}>{props.children}</button>
+}
+
+export const ButtonLightPurple=(props:any) => {
+    return <button className="button-light-purple" onClick={props.onClick}>{props.children}</button>
 }
