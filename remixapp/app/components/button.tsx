9d5460e0cde3d334e139a5a1dc9046f7ad51a75f
CLARISSA CHEN <clarissachen5@gmail.com>
2023-11-19 09:43:04 -0500

    finished yourprofile page

--- remixapp/app/components/button.tsx
+++ remixapp/app/components/button.tsx
@@ -1,5 +1,5 @@
 export const Button=(props:any) => {
-    return <button className={`button button-${props.color} button-${props.size}`} onClick={props.onClick}>{props.children}</button>
+    return <button className={`button same-line button-${props.color} button-${props.size}`} onClick={props.onClick}>{props.children}</button>
 }
 
 // export const ButtonLightPurple=(props:any) => {
--- remixapp/app/routes/YourProfile.tsx
+++ remixapp/app/routes/YourProfile.tsx
@@ -11,13 +11,56 @@ function YourProfile() {
     <div  className="center-container">
       <h1 className='welcome'>Anne Lauve</h1>
       <h3 className='little-text'>computer science, class of 2027</h3> 
-      <h3 className='sectiion'>Active Tables</h3>
+     </div>
 
-      <Button onClick={() =>navigate("/Profile")}></Button>
+     <h3 className='section'>Active Tables</h3>
+      <div className='dark-green-box'>
+        
+          <Button color="white" size='large' onClick={() =>navigate("/Profile")}>Warren Dining Hall (table for 4)</Button>
+    
+        {/* <div className='center-container'> */}
+          {/* </div> */}
+      </div>
+
+        
+
+
+     <h3 className='section'>Interests</h3>
+      <div className='dark-green-box'>
+        
+          <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>art</Button>
+          <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>hackathons</Button>
+          <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>comic books</Button>
+          <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>kpop</Button>
+          {/* <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>makeup</Button>
+          <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>computer science</Button>
+          <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>skincare</Button>
+          <Button color="light-green" size='tiny' onClick={() =>navigate("/Profile")}>acapella</Button> */}
     
     
-      <Button color="light-green" size="small" onClick={() =>navigate("/Profile")}>Home</Button>
-    </div></div>
+        {/* <div className='center-container'> */}
+          {/* </div> */}
+      </div>
+
+      <h3 className='section'>Top Spendings</h3>
+      <div className='dark-green-box'>
+        <div className='higher-text'><h5>1. Popmart</h5>
+          <h5>2. Basho Express</h5>
+          <h5>3. Ticketmaster</h5></div>
+          
+    
+        {/* <div className='center-container'> */}
+          {/* </div> */}
+      </div>
+        <div className='center-container'>
+        <Button color="light-green" size="small" onClick={() =>navigate("/Profile")}>Home</Button>
+      
+        </div>
+      
+    
+      
+    </div>
+
   );
 }
 
